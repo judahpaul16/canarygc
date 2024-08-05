2f1d9f794ff712c13e7b92a8cc011d98526fbc4d
Judah Paul <me@judahpaul.com>
2024-08-05 18:35:15 -0400

    Map Stuff

--- src/frontend/src/components/FlightPlanSettings.svelte
+++ src/frontend/src/components/FlightPlanSettings.svelte
@@ -1,9 +1,68 @@
+<script lang="ts">
+    import { mapStore, mavLocationStore } from '../stores/mapStore';
+    import { flightPlanStore } from '../stores/flightPlanStore';
+    
+    let actions: { [key: number]: {
+        type: string;
+        lat: number;
+        lon: number;
+        altitude: number;
+        notes: string;
+        notify: boolean;
+    }} = {};
+
+    flightPlanStore.subscribe((value) => {
+        actions = value;
+    });
+    
+    let map: L.Map | null = null;
+    let mavLocation: L.LatLng | null = null;
+
+    mapStore.subscribe((value: L.Map | null) => {
+        if (value) {
+            map = value;
+        }
+    });
+
+    mavLocationStore.subscribe((value) => {
+        mavLocation = value;
+    });
+
+    async function removeAllActions() {
+        let L = await import('leaflet');
+        let confirmed = confirm('Are you sure you want to clear the flight plan?');
+        if (confirmed) {
+            map!.eachLayer((layer) => {
+                if (layer instanceof L.Marker || layer instanceof L.Polyline) {
+                    map!.removeLayer(layer);
+                }
+            });
+            const icon = L.icon({
+                iconUrl: 'map/here.png',
+                iconSize: [45, 45],
+                iconAnchor: [23, 45],
+                popupAnchor: [0, -45],
+                shadowSize: [41, 41]
+            });
+            L.marker([mavLocation?.lat || 33.749, mavLocation?.lng || -84.388], {icon: icon}).addTo(map!);
+
+            document.querySelectorAll('.action-container').forEach((el) => {
+                el.remove();
+            });
+
+            mapStore.set(map);
+            flightPlanStore.set({});
+        }
+    }
+    
+</script>
+
 <section class="flight-plan-settings bg-[#1c1c1e] rounded-lg p-4 h-full text-white">
     <button>
         <i class="fas fa-save"></i>
         Save Flight Plan
     </button>
-    <button>
+    <button on:click={removeAllActions}>
         <i class="fas fa-trash-alt"></i>
         Clear Flight Plan
     </button>
