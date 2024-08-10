8d91c85be8d033b29abcb34e328dd73565b2faa0
Judah Paul <me@judahpaul.com>
2024-08-10 18:45:57 -0400

    Hide navbar on login and main route
    + Updated main route with useful documentation

--- src/frontend/src/routes/+page.svelte
+++ src/frontend/src/routes/+page.svelte
@@ -10,7 +10,7 @@
 </sveltekit:head>
 
 <div class="flex items-center justify-center h-full">
-  <div class="bg-gray-900 bg-opacity-75 text-white p-8 rounded-lg shadow-lg max-w-md w-full">
+  <div class="bg-[#121212bd] text-white p-8 rounded-2xl shadow-lg max-w-md w-full">
     <h1 class="text-3xl font-bold mb-4 text-center">MAV Manager GCS</h1>
     {#if user}
       <p class="text-lg text-center">Hello, {user?.record?.name || 'admin'}!
@@ -19,42 +19,43 @@
     {:else}
       <p class="text-lg text-center">You are not logged in. Please <a href="/login" class="text-blue-500 underline">login</a>.</p>
     {/if}
-    <!-- Changelog -->
-    <div class="mt-4 changelog">
-      <h2 class="text-xl font-bold mb-2">Changelog</h2>
-      <ul class="styled-list">
-        <li><em>Added login + dashboard page</em></li>
-        <li><em>Added login/logout functionality</em></li>
-        <li><em>Added interactive map</em></li>
-      </ul>
-    </div>
-    <!-- Work In Progress -->
-    <div class="mt-4 wip">
-      <h2 class="text-xl font-bold mb-2">Work In Progress</h2>
-      <ul class="styled-list">
-        <li><em>Get live video feed</em></li>
-        <li><em>Get live stats</em></li>
-        <li><em>Get live GPS data</em></li>
-        <li><em>Get/Set flight route and get progress</em></li>
-        <li><em>Implement flight controls</em></li>
-        <li><em>Fleet Management: Self-Host vs SaaS</em></li>
-      </ul>
-    </div>
+    <!-- Useful Documentation -->
+      <!-- MAVLink -->
+      <div class="mt-4 mavlink">
+        <h2 class="text-lg font-bold mb-2">MAVLink</h2>
+        <ul class="styled-list">
+          <li><a href="https://mavlink.io/en/" target="_blank">MAVLink</a></li>
+          <li><a href="https://mavlink.io/en/messages/common.html" target="_blank">Common MAVLink Messages</a></li>
+          <li><a href="https://ardupilot.org/copter/docs/mission-command-list.html" target="_blank">Copter Mission Command List</a></li>
+          <li><a href="https://ardupilot.org/planner/docs/common-mavlink-mission-command-messages-mav_cmd.html" target="_blank">Common MAVLink Mission Command Messages</a></li>
+          <li><a href="https://ardupilot.org/copter/docs/auto-mode.html" target="_blank">Auto Mode</a></li>
+          <li><a href="https://ardupilot.org/planner/docs/common-loading-firmware-onto-pixhawk.html" target="_blank">Loading Firmware onto Pixhawk</a></li>
+        </ul>
+      </div>
+      <!-- Safety -->
+      <div class="mt-4 safety">
+        <h2 class="text-lg font-bold mb-2">Safety</h2>
+        <ul class="styled-list">
+          <li><a href="https://dronesafetymap.com" target="_blank">Drone Safety Map</a></li>
+          <li><a href="https://ardupilot.org/copter/docs/common-prearm-safety-checks.html" target="_blank">Prearm Safety Checks</a></li>
+          <li><a href="https://www.airsight.com/blog/faa-rules-and-regulations-for-unmanned-aircraft-systems-uas" target="_blank">FAA Rules and Regulations for UAS</a></li>
+        </ul>
+      </div>
   </div>
 </div>
 
 <style>
-  .changelog {
+  .mavlink {
     border-left: 3px solid #58a6ff;
     padding-left: 1rem;
   }
 
-  .wip {
+  .safety {
     border-left: 3px solid #ffcb5a;
     padding-left: 1rem;
   }
 
-  .changelog h2, .wip h2 {
+  .mavlink h2, .safety h2 {
     margin-top: 0;
   }
 
@@ -77,5 +78,13 @@
     left: 0;
     color: #58a6ff;
     font-size: 1.2rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: #58a6ff;
+  }
+  a:hover {
+    color: white;
   }
 </style>
