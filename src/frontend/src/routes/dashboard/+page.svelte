e38159fc7d463e03a2a49e6359322261e72bae90
Judah Paul <me@judahpaul.com>
2024-08-11 03:23:13 -0400

    Styling Tweaks
    Former-commit-id: 3f90f625846a7104f1af513f9d10a47b6249717a

--- src/frontend/src/routes/dashboard/+page.svelte
+++ src/frontend/src/routes/dashboard/+page.svelte
@@ -17,7 +17,7 @@
 </sveltekit:head>
 
 <div class="dashboard-container h-full flex items-center justify-center min-h-[95vh] p-0">
-  <div class="dashboard w-full grid grid-cols-12 grid-rows-7 gap-4 p-5 bg-[#121212] rounded-[30px] rounded-l-none overflow-auto max-h-[90vh]">
+  <div class="dashboard w-full grid grid-cols-12 grid-rows-7 gap-4 p-5 bg-[#121212] rounded-[30px] rounded-l-none overflow-auto h-[90vh] max-h-[720px]">
     <div class="live-feed col-span-6 row-span-4">
       <LiveFeed />
     </div>
@@ -62,6 +62,7 @@
       max-height: 92vh;
       border-radius: 0;
       overflow-y: auto;
+      height: 100%;
     }
 
     .dashboard > * {
