13aef8060ce54e018e57d5c2dbd28a253a49435f
Judah Paul <me@judahpaul.com>
2024-09-06 12:50:20 -0400

    Use built-in fullscreen button for LiveFeed

--- src/frontend/src/components/LiveFeed.svelte
+++ src/frontend/src/components/LiveFeed.svelte
@@ -79,12 +79,9 @@
 >
   <div class="container w-full h-full relative">
     <img id="no-signal" src={darkMode ? 'no-signal.gif': 'no-signal-light.gif'} alt="No Signal" class="absolute top-0 w-full h-full object-cover rounded-lg z-10" />
-    <video id="live-feed" autoplay loop muted class="bg-black absolute top-0 w-full h-full object-cover rounded-lg z-0"></video>
+    <video id="live-feed" controls autoplay loop muted class="bg-black absolute top-0 w-full h-full object-cover rounded-lg z-0"></video>
     <div class="tab absolute top-2 left-2 bg-[#f24e4eb9] text-[#ffffff] text-md px-2 py-1 rounded-full z-20">Live Feed</div>
-    <div class="caution-text text-md absolute bottom-0 left-0 bg-[#252525cf] px-2 py-1 rounded-tr-lg rounded-lg rounded-br-none rounded-tl-none z-20">Use Caution: The feed may be slightly delayed.</div>
-    <button class="absolute top-2 right-2 text-[#ffffff]bg-opacity-75 p-2 px-3 rounded-full z-20" on:click={handleFullScreen}>
-      <i class="fas fa-expand"></i>
-    </button>
+    <div class="caution-text opacity-0 text-md absolute top-2 right-2 bg-[#252525cf] px-2 py-1 rounded-full z-20">Use Caution: The feed may be slightly delayed.</div>
   </div>
 </div>
 
@@ -92,6 +89,10 @@
   #live-feed-container {
     background-color: var(--primaryColor);
     border: 10px solid var(--primaryColor);
+  }
+
+  #live-feed-container:hover .caution-text {
+    opacity: 1;
   }
 
   #no-signal {
@@ -104,19 +105,7 @@
 
   .caution-text {
     color: var(--fontColor);
-    background-color: var(--primaryColor);
-    border: 2px solid var(--secondaryColor);
-    opacity: 0.75;
-  }
-
-  button {
-    color: var(--fontColor);
-    background-color: var(--secondaryColor);
-    border: 2px solid var(--primaryColor);
-    opacity: 0.75;
-  }
-
-  button:hover {
-    opacity: 0.65;
+    background-color: rgb(from var(--primaryColor) r g b / 75%);
+    border: 2px solid rgb(from var(--secondaryColor) r g b / 75%);
   }
 </style>
