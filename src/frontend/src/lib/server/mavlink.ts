c918e26c1a6f0c70bbb87c7399dec71669a5b5c4
Judah Paul <me@judahpaul.com>
2024-09-08 17:44:19 -0400

    Update mavlink.ts
    Former-commit-id: c80a6e5d99da563dac24624fc826c83ccf0280c9 [formerly 2857277f0b1ecf62f6a7c71a681c8a9385aadd5a] [formerly 7f3c167523e21afc17037d7270664a96c444be2e [formerly 2d9fd88e3ae260a4cb81295a76cf4dbf3d1281e3]]
    Former-commit-id: 7f3c167523e21afc17037d7270664a96c444be2e
    Former-commit-id: 925e822e391576e57522278ae0349aa3a541f1b5

--- src/frontend/src/lib/server/mavlink.ts
+++ src/frontend/src/lib/server/mavlink.ts
@@ -46,7 +46,7 @@ async function closeExistingConnection(): Promise<void> {
 
 async function openNewConnection(): Promise<void> {
     let isProduction = process.env.NODE_ENV === 'production';
-    if (isProduction) {
+    if (isProduction === true) {
         // Use UART serial port in production
         port = new SerialPort({ path: '/dev/ttyACM0', baudRate: 115200, lock: false });
     } else {
