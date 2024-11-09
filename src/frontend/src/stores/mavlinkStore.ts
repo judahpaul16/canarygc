bbef53a1b09e35809b283edf514a2c8eafb12754
Judah Paul <me@judahpaul.com>
2024-11-09 00:43:38 -0500

    Created Parameter Config page

--- src/frontend/src/stores/mavlinkStore.ts
+++ src/frontend/src/stores/mavlinkStore.ts
@@ -3,6 +3,22 @@ import { writable } from 'svelte/store';
 let L: typeof import('leaflet');
 let latLng = { lat: 33.7909, lng: -84.388 };
 
+export interface Parameter {
+    param_id: string;
+    param_value: number;
+    param_type: number;
+    param_count: number;
+    param_index: number;
+}
+
+export interface ParameterMeta {
+    min?: number;
+    max?: number;
+    increment?: number;
+    description?: string;
+    units?: string;
+}
+
 export const onlineStore = writable<boolean>(false);
 export const mavModelStore = writable<string>('UNKNOWN');
 export const mavTypeStore = writable<string>('Unknown');
@@ -16,3 +32,4 @@ export const mavAltitudeStore = writable<number>(0);
 export const mavSpeedStore = writable<number>(0);
 export const mavBatteryStore = writable<number|null>(null);
 export const mavlinkLogStore = writable<string[]>([]);
+export const mavlinkParamStore = writable<{[key: string]: Parameter}>({});
