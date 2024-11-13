06d9f4181b4098fc2d340ba5bd93a958d379095a
Judah Paul <me@judahpaul.com>
2024-11-12 19:00:10 -0500

    Added rotate button

--- src/frontend/src/components/LiveFeed.svelte
+++ src/frontend/src/components/LiveFeed.svelte
@@ -64,18 +64,29 @@
     }
   }
 
+  function rotateVideo() {
+    const iframe = document.querySelector('#live-feed') as HTMLIFrameElement;
+    if (!iframe) return;
+
+    iframe.style.transform = iframe.style.transform === 'rotate(90deg)' ? 'rotate(0deg)' : 'rotate(90deg)';
+  }
+
   onMount(() => {
     const fetchLiveFeed = async () => {
+
       let response = await fetch(`http://${window.location.hostname}:8889/cam`);
+      const liveFeed = document.getElementById('live-feed') as HTMLIFrameElement;
+      
       if (response.status === 200) {
-        const liveFeed = document.getElementById('live-feed') as HTMLIFrameElement;
         liveFeed.style.zIndex = '20';
+      } else {
+        liveFeed.style.zIndex = '0';
       }
       
       adjustVideoSize();
     };
 
-    fetchLiveFeed();
+    setInterval(() => fetchLiveFeed(), 5000);
 
     window.addEventListener('resize', adjustVideoSize);
 
@@ -96,6 +107,9 @@
     <iframe allowfullscreen id="live-feed" title="Live Feed" src={ typeof window !== 'undefined' ? `http://${window.location.hostname}:8889/cam`  : ''} class="bg-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover rounded-lg z-0"></iframe>
     <div class="tab absolute top-2 left-2 bg-[#f24e4eb9] text-[#ffffff] text-md px-2 py-1 rounded-full z-20">Live Feed</div>
     <div class="caution-text opacity-[50%] text-md absolute bottom-2 left-2 bg-[#252525cf] px-2 py-1 mr-[0.5em] rounded-full z-20">Use Caution: The feed may be slightly delayed.</div>
+    <button class="absolute top-2 right-14 p-2 px-[14px] rounded-full z-20 opacity-[60%]" on:click={rotateVideo}>
+      <i class="fas fa-sync-alt"></i>
+    </button>
     <button class="absolute top-2 right-2 p-2 px-[14px] rounded-full z-20 opacity-[60%]" on:click={handleFullScreen}>
       <i class="fas fa-expand"></i>
     </button>
