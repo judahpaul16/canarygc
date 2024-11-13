4ac0ba6a200a58cb1e7932fbb976c486adb88be4
Judah Paul <me@judahpaul.com>
2024-11-13 10:43:32 -0500

    Updated rotate logic

--- src/frontend/src/components/LiveFeed.svelte
+++ src/frontend/src/components/LiveFeed.svelte
@@ -67,8 +67,12 @@
   function rotateVideo() {
     const iframe = document.querySelector('#live-feed') as HTMLIFrameElement;
     if (!iframe) return;
-
-    iframe.style.transform = iframe.style.transform === 'rotate(90deg)' ? 'rotate(0deg)' : 'rotate(90deg)';
+    
+    if (String(iframe.style.transform).includes('rotate(180deg)')) {
+      iframe.style.transform = 'translate(-50%, -50%) rotate(0deg)';
+    } else {
+      iframe.style.transform = 'translate(-50%, -50%) rotate(180deg)';
+    }
   }
 
   onMount(() => {
@@ -104,7 +108,7 @@
 >
   <div class="container w-full h-full relative">
     <img id="no-signal" src="no-signal.gif" alt="No Signal" class="absolute top-0 w-full h-full object-cover z-10" />
-    <iframe allowfullscreen id="live-feed" title="Live Feed" src={ typeof window !== 'undefined' ? `http://${window.location.hostname}:8889/cam`  : ''} class="bg-black absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 object-cover rounded-lg z-0"></iframe>
+    <iframe allowfullscreen id="live-feed" title="Live Feed" src={ typeof window !== 'undefined' ? `http://${window.location.hostname}:8889/cam`  : ''}></iframe>
     <div class="tab absolute top-2 left-2 bg-[#f24e4eb9] text-[#ffffff] text-md px-2 py-1 rounded-full z-20">Live Feed</div>
     <div class="caution-text opacity-[50%] text-md absolute bottom-2 left-2 bg-[#252525cf] px-2 py-1 mr-[0.5em] rounded-full z-20">Use Caution: The feed may be slightly delayed.</div>
     <button class="absolute top-2 right-14 p-2 px-[14px] rounded-full z-20 opacity-[60%]" on:click={rotateVideo}>
@@ -145,6 +149,14 @@
     width: 300%;
     height: 300%;
     pointer-events: none;
+    background-color: #000;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    object-fit: cover;
+    border-radius: 0.5rem;
+    z-index: 0;
   }
 
   button {
