a33fc52e0596f6fcae91eb3d7dcc0e1ef287931d
razsa <21493e@gmail.com>
2025-01-02 22:28:37 -0500

    \o/

---
+++ src/backend/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"embed"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/razsa/go-auth/routes"
+)
+
+//go:embed fe/*
+var fe embed.FS
+
+func main() {
+
+	app := fiber.New()
+
+	// Setup routes
+	routes.Setup(app)
+
+	// Serve embedded frontend files
+	app.Use("/", filesystem.New(filesystem.Config{
+		Root:       http.FS(fe),
+		PathPrefix: "fe",
+		Browse:     true,
+	}))
+
+	app.Listen(":8000")
+}
